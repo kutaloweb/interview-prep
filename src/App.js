954cd8de97725300ea4d2e32d372b5099f4df790
alexey <kutalo84@gmail.com>
2020-11-03 03:54:48 -0800

    Add Make Todo List

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Link, Route, Switch} from 'react-router-dom'
+import TodoListClasses from "./components/TodoListClasses"
+import TodoListHooks from "./components/TodoListHooks"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Switch>
+                <Route exact path="/todo-list-classes" component={TodoListClasses}/>
+                <Route exact path="/todo-list-hooks" component={TodoListHooks}/>
+            </Switch>
+            <h3>React Interview Questions</h3>
+            <div>
+                1. Make Todo List <Link to="/todo-list-classes">TodoListClasses</Link> <Link to="/todo-list-hooks">TodoListHooks</Link>
+            </div>
+        </div>
+    );
 }
 
 export default App;
